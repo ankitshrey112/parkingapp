bbc3946f914de95ad966fcfd17439db9c6df6fb7
Ankit Shrey <ankit.shrey@cogoport.com>
2023-08-31 17:00:05 +0530

    controller: permit params

--- app/controllers/carparks_controller.rb
+++ app/controllers/carparks_controller.rb
@@ -7,14 +7,23 @@ class CarparksController < ApplicationController
     get_service_respone(__method__.to_s)
   end
 
+  private
+
   # donot change below method, just add new controllers above and implement new interaction in services with same name.
   def get_service_respone(request)
-    api_request = request.camelize.constantize.run(params.to_h.as_json.deep_symbolize_keys)
+    interaction = request.camelize.constantize
+    @required_params = interaction.class.inputs.keys.map(&:to_sym)
+
+    api_request = interaction.run(self.permitted_params.to_h.as_json.deep_symbolize_keys)
 
     if api_request.errors.present?
       render json: { errors: api_request.errors.full_messages }, status: :bad_request
     end
 
     render json: api_request.result, status: :ok
+  end
+
+  def permitted_params
+    params.permit(@required_params)
   end
 end
