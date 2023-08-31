6e5dc23391d43a81324ff83aa66e3749133d4e66
Ankit Shrey <ankit.shrey@cogoport.com>
2023-08-31 16:10:33 +0530

    database changes - change uuid to binary

--- db/migrate/20230830142658_create_carpark_availability.rb
+++ db/migrate/20230830142658_create_carpark_availability.rb
@@ -1,7 +1,7 @@
 class CreateCarparkAvailability < ActiveRecord::Migration[7.0]
-  def self.uo
+  def self.up
     create_table :carpark_availabilities, id: :binary, limit: 16 do |t|
-      t.uuid :carpark_id, null: false
+      t.binary :carpark_id, null: false, limit: 16
       t.string :lot_type
       t.integer :total_lots
       t.integer :lots_available, null: false
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,16 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_08_30_142658) do
+  create_table "carpark_availabilities", id: { type: :binary, limit: 16 }, charset: "latin1", force: :cascade do |t|
+    t.binary "carpark_id", limit: 16, null: false
+    t.string "lot_type"
+    t.integer "total_lots"
+    t.integer "lots_available", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["carpark_id"], name: "index_carparks_on_carpark_number", unique: true
+  end
+
   create_table "carparks", id: { type: :binary, limit: 16 }, charset: "latin1", force: :cascade do |t|
     t.string "carpark_number", null: false
     t.float "latitude", null: false
