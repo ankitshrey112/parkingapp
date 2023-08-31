2fe4b8f3bda85bcca2cc6b0f8e474954b816cf83
Ankit Shrey <ankit.shrey@cogoport.com>
2023-08-31 15:39:48 +0530

    add indexes to tables

--- db/migrate/20230830140024_carpark.rb
+++ db/migrate/20230830140024_carpark.rb
@@ -1,9 +1,9 @@
 class Carpark < ActiveRecord::Migration[7.0]
   def change
     create_table :carparks, id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
-      t.string :carpark_number
-      t.float :latitude
-      t.float :longitude
+      t.string :carpark_number, null: false
+      t.float :latitude, null: false
+      t.float :longitude, null: false
       t.string :carpark_type
       t.string :type_of_parking_system
       t.string :short_term_parking
--- db/migrate/20230830142658_create_carpark_availability.rb
+++ db/migrate/20230830142658_create_carpark_availability.rb
@@ -1,14 +1,14 @@
 class CreateCarparkAvailability < ActiveRecord::Migration[7.0]
   def change
     create_table :carpark_availabilities, id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
-      t.string :carpark_number, null: false
+      t.uuid :carpark_id, null: false
       t.string :lot_type
       t.integer :total_lots
       t.integer :lots_available, null: false
 
       t.timestamps
 
-      t.index [:carpark_number], name: 'index_carparks_on_carpark_number', unique: true
+      t.index [:carpark_id], name: 'index_carparks_on_carpark_number', unique: true
     end
   end
 end
