268b9116be3e4fae2aeecd31811674cb6111a1d4
Ankit Shrey <ankit.shrey@cogoport.com>
2023-08-31 16:25:56 +0530

    abstract controllers

--- app/controllers/carparks_controller.rb
+++ app/controllers/carparks_controller.rb
@@ -1,6 +1,11 @@
 class CarparksController < ApplicationController
   def get_nearest_availabilities
-    api_request = GetNearestAvailabilities.run(params.as_json.map(&:deep_symbolize_keys))
+    get_service_respone(__method__.to_s)
+  end
+
+  # donot change below method, just add new controllers above and implement new interaction in services with same name.
+  def get_service_respone(request)
+    api_request = request.camelize.constantize.run(params.to_h.as_json.deep_symbolize_keys)
 
     if api_request.errors.present?
       render json: { errors: api_request.errors.full_messages }, status: :bad_request
