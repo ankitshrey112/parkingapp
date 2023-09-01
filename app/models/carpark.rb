a2766ea543f03fbc33447f5617e0593d48d17fd8
Ankit Shrey <ankit.shrey@cogoport.com>
2023-09-01 10:23:59 +0530

    added validations

--- app/models/carpark.rb
+++ app/models/carpark.rb
@@ -4,5 +4,7 @@ class Carpark < ApplicationRecord
 
   has_one :availability, class_name: "CarparkAvailability", :foreign_key => :carpark_number, :primary_key => :carpark_number, required: false
 
+  validates_presence_of :carpark_number, :latitude, :longitude
+
   validates_uniqueness_of :carpark_number
 end
--- app/models/carpark_availability.rb
+++ app/models/carpark_availability.rb
@@ -1,5 +1,7 @@
 class CarparkAvailability < ApplicationRecord
   belongs_to :carpark, class_name: "Carpark", :foreign_key => :carpark_number, :primary_key => :carpark_number, required: false
 
+  validates_presence_of :carpark_number, :available_lots
+
   validates_uniqueness_of :carpark_number
 end
