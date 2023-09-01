0c87cba703823b10b086cd18d2e50ccbfeef782d
Ankit Shrey <ankit.shrey@cogoport.com>
2023-09-01 09:23:13 +0530

    empty schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,35 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_30_142658) do
-  create_table "carpark_availabilities", charset: "latin1", force: :cascade do |t|
-    t.string "carpark_number", null: false
-    t.string "lot_type"
-    t.integer "total_lots"
-    t.integer "available_lots", null: false
-    t.datetime "update_datetime"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["carpark_number"], name: "index_carpark_availabilities_on_carpark_number", unique: true
-  end
-
-  create_table "carparks", charset: "latin1", force: :cascade do |t|
-    t.string "carpark_number", null: false
-    t.string "address"
-    t.decimal "latitude", precision: 10, scale: 4, null: false
-    t.decimal "longitude", precision: 10, scale: 4, null: false
-    t.string "carpark_type"
-    t.string "type_of_parking_system"
-    t.string "short_term_parking"
-    t.string "free_parking"
-    t.string "night_parking"
-    t.integer "car_park_decks"
-    t.decimal "gantry_height", precision: 10, scale: 4
-    t.string "car_park_basement"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["carpark_number"], name: "index_carparks_on_carpark_number", unique: true
-    t.index ["latitude"], name: "index_carparks_on_carpark_lat_long"
-  end
-
+ActiveRecord::Schema[7.0].define(version: 0) do
 end
