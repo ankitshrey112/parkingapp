60d3d497e4988dea8f80ceee557f9ec8c79bec3d
Ankit Shrey <ankit.shrey@cogoport.com>
2023-09-01 09:17:52 +0530

    optional relations

--- app/models/carpark.rb
+++ app/models/carpark.rb
@@ -2,7 +2,7 @@ class Carpark < ApplicationRecord
   geocoded_by :address
   after_validation :geocode
 
-  has_one :availability, class_name: "CarparkAvailability", :foreign_key => :carpark_number, :primary_key => :carpark_number, optional: true
+  has_one :availability, class_name: "CarparkAvailability", :foreign_key => :carpark_number, :primary_key => :carpark_number, required: false
 
   validates_uniqueness_of :carpark_number
 end
--- app/models/carpark_availability.rb
+++ app/models/carpark_availability.rb
@@ -1,5 +1,5 @@
 class CarparkAvailability < ApplicationRecord
-  belongs_to :carpark, class_name: "Carpark", :foreign_key => :carpark_number, :primary_key => :carpark_number, optional: true
+  belongs_to :carpark, class_name: "Carpark", :foreign_key => :carpark_number, :primary_key => :carpark_number, required: false
 
   validates_uniqueness_of :carpark_number
 end
