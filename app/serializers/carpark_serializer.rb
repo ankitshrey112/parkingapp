60ff29eaab5892991442fcd84a039717ac895065
ankitshrey112 <59553900+ankitshrey112@users.noreply.github.com>
2023-09-07 23:37:57 +0530

    Delete app/serializers/carpark_serializer.rb

--- app/serializers/carpark_serializer.rb
+++
@@ -1,7 +0,0 @@
-class CarparkSerializer < ActiveModel::Serializer
-  attributes :address, :latitude, :longitude, :total_lots, :available_lots, :distance
-
-  def distance
-    object.distance.round(2)
-  end
-end
