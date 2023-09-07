53e9c62ff01aa29674fc04c0a8e7e52d1cb45ea6
ankitshrey112 <59553900+ankitshrey112@users.noreply.github.com>
2023-09-07 23:38:25 +0530

    Delete app/serializers/get_nearest_availabilities_serializer.rb

--- app/serializers/get_nearest_availabilities_serializer.rb
+++
@@ -1,5 +0,0 @@
-class GetNearestAvailabilitiesSerializer < ActiveModel::Serializer
-  attributes :list, :current_page, :current_count, :total_pages, :total_count
-
-  has_many :list, serializer: CarparkSerializer
-end
